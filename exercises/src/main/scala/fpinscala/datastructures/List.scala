5ab3b324250a13bab6ff6599a56a1a66a9e8735d
tydunn <tydunn@umich.edu>
2018-08-10 07:29:38 -0400

    dropWhile

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -53,14 +53,27 @@ object List { // `List` companion object. Contains functions for creating and wo
   def tail[A](l: List[A]): List[A] = 
     l match {
       case Nil => sys.error("tail of empty list")
-      case Cons(_, t) => t
+      case Cons(_,t) => t
     }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil => sys.error("setHead on empty list")
+      case Cons(_,t) => Cons(h,t)
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_,t) => drop(t,n-1)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h,t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
 
   def init[A](l: List[A]): List[A] = ???
 

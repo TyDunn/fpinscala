3f5259d08ddcd60fcefba75847817a92826c4b8b
tydunn <tydunn@umich.edu>
2018-12-10 08:16:42 -0500

    double

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -68,6 +68,9 @@ object RNG {
     }
   }
 
+  def _double: Rand[Double] =
+    map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
+
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???

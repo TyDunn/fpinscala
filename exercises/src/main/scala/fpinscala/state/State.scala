d0cb1f42e76b58d765fcdf6e7b1ee90897f16263
Ty Dunn <tydunn@umich.edu>
2019-01-11 07:30:08 -0500

    reimplementing functions using flatMap

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -106,11 +106,12 @@ object RNG {
 
 case class State[S,+A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
-    ???
+    flatMap(a => unit(f(a)))
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
+    flatMap(a => sb.map(b => f(a, b)))
   def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    val (a, s1) = run(s)
+    f(a).run(s1)
 }
 
 sealed trait Input
@@ -120,6 +121,16 @@ case object Turn extends Input
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
+
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    def go(s: S, actions: List[State[S, A]], acc: List[A]): (List[A],S) =
+      actions match {
+        case Nil => (acc.reverse, s)
+        case h :: t => h.run(s) match { case(a,s2) => go(S2, t, a, a :: acc) }
+      }
+    State((s: S) => go(s, sas, List()))
+  }
+
   type Rand[A] = State[RNG, A]
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 }
